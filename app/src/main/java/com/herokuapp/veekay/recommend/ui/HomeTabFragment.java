5d4d7b9136d6bc1740bd3535c6b673b60e3c1f55
ngenovictor <ngenovictor321@gmail.com>
2017-10-04 11:08:57 +0300

    add new ask a question activity

--- app/src/main/java/com/herokuapp/veekay/recommend/ui/HomeTabFragment.java
+++ app/src/main/java/com/herokuapp/veekay/recommend/ui/HomeTabFragment.java
@@ -2,6 +2,7 @@ package com.herokuapp.veekay.recommend.ui;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
